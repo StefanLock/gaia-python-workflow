61c4f43b511d8929662980f697a41b88c2b90868
Stefan Lock <slock@eu-slock.home>
2022-10-25 09:35:54 +0100

    troubleshooting

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -5,27 +5,33 @@ import logging
 
 def get_template(args):
 
+    acceptable_args = ["dev", "staging", "prod"]
+
     for x in args:
         if x == "":
             pass
         else:
-            logging.info(f"Environment selected: {x.value}")
-
-            mylist = """
-                monitoring-server: {env}-grafana.com
-                key-store:  {env}-vault.com
-                web-service-frontend: {env}-frontend.com
-                db-service-backend: {env}-db.com""".format(env=x.value)
-
-            try:
-                config= open("/tmp/config.yaml","a+")
-                for f in mylist:
-                    config.write(f)
-                config.close()
-                logging.info("Created the config file successfully")
-            except:
-                logging.error("Failed to create config")
+            if x.value not in acceptable_args:
+                logging.error(f"Unsupported environment selected: {x.value}")
                 exit(1)
+            else:
+                logging.info(f"Environment selected: {x.value}")
+
+                mylist = """
+                    monitoring-server: {env}-grafana.com
+                    key-store:  {env}-vault.com
+                    web-service-frontend: {env}-frontend.com
+                    db-service-backend: {env}-db.com""".format(env=x.value)
+
+                try:
+                    config= open("/tmp/config.yaml","a+")
+                    for f in mylist:
+                        config.write(f)
+                    config.close()
+                    logging.info("Created the config file successfully")
+                except:
+                    logging.error("Failed to create config")
+                    exit(1)
 
             test = os.popen('cat /tmp/config.yaml').read()
             logging.info(test)
