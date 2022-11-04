f3c386ed816995455ea2318a7a5f371970d39b48
Stefan Lock <slock@eu-slock.local>
2022-11-04 12:06:56 +0000

    testing

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -46,7 +46,8 @@ def s3_Upload(args):
 
     logging.info(ACCESS_KEY)
     logging.info(SECRET_KEY)
-    client = boto3.Session(
+    
+    client = boto3.client(
         's3',
         aws_access_key_id=ACCESS_KEY,
         aws_secret_access_key=SECRET_KEY,
